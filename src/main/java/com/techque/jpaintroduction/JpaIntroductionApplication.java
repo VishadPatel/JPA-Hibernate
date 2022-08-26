472cb42b2d56b2ad14a13a1b70d33baa349aa35d
Vishad Patel <pat.vishad@gmail.com>
2022-08-26 10:27:52 +0530

    Initial commit

---
+++ src/main/java/com/techque/jpaintroduction/JpaIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.techque.jpaintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaIntroductionApplication.class, args);
+	}
+
+}
